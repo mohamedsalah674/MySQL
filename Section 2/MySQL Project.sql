224196b1e041fad5ec8c47cc2c69e09dcb48ebb5
Mohamed Salah <66376551+mohamedsalah674@users.noreply.github.com>
2023-11-13 10:20:19 +0200

    Create MySQL Project.sql
    Code of section 2 :
    
    1- Basic commands like (create , delete , alter , user ,show , select and truncate)
    2-  Operators (and , or , not , in , between and like)

---
+++ Section 2/MySQL Project.sql
@@ -0,0 +1,42 @@
+create database MyFirstSQL;
+
+use MyFirstSQL;
+
+show databases;
+
+
+show databases;
+
+create table student_table(
+student_id INT,
+student_name VARCHAR(100),
+course_name VARCHAR(40)
+);
+
+show tables ;
+
+describe student_table;
+
+alter table student_table add email VARCHAR(100);
+
+describe student_table ;
+
+truncate table student_table ;
+
+insert into student_table (student_id , student_name , course_name , email)
+values (1 , "Mohamed Salah" , "System Analysis" , "Mohamed@gmail.com");
+
+insert into student_table (student_id , student_name , course_name , email)
+values (1 , "Ahmed Moamen" , "Electric Circuits" , "Ahmed@gmail.com");
+
+insert into student_table (student_id , student_name , course_name , email)
+values (1 , "Fatma Salah" , "Network" , "Fatma@gmail.com");
+
+select * from student_table;
+
+select * from student_table where email = "Mohamed@gmail.com"; 
+
+
+
+
+
